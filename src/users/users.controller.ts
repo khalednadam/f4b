2f709a826ae07bedcdfeb5b0242b377982e0f8ae
Khaled Nadam <khalednadam@gmail.com>
2024-08-06 21:08:16 +0300

    fix(user): add controller return types

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -11,18 +11,21 @@ import {
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './user.entity';
+import { Pagination } from 'nestjs-typeorm-paginate';
+import { UpdateResult } from 'typeorm';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post('register')
-  create(@Body() createUserDto: CreateUserDto) {
+  create(@Body() createUserDto: CreateUserDto): Promise<User> {
     return this.usersService.create(createUserDto);
   }
 
   @Get('user/:id')
-  async getUserById(@Param('id') id: number) {
+  async getUserById(@Param('id') id: number): Promise<User> {
     const user = await this.usersService.getUserById(id);
     if (!user) {
       throw new NotFoundException('user not found');
@@ -34,14 +37,15 @@ export class UsersController {
   getAllUsers(
     @Param('page') page: number = 1,
     @Param('limit') limit: number = 20,
-  ) {
+  ): Promise<Pagination<User>> {
     return this.usersService.paginate({ page, limit });
   }
 
   @Delete(':id')
-  async deleteUser(@Param('id') id: number) {
-    const deleteUser = await this.usersService.deleteUser(id);
-    if (deleteUser.affected === 0) {
+  async deleteUser(@Param('id') id: number): Promise<User> {
+    const deleteUser = await this.usersService.getUserById(id);
+    const deleteResult = await this.usersService.deleteUser(id);
+    if (deleteResult.affected === 0) {
       throw new NotFoundException('user not found');
     }
     return deleteUser;
@@ -51,7 +55,7 @@ export class UsersController {
   async updateUser(
     @Param('id') id: number,
     @Body() updateUserDto: UpdateUserDto,
-  ) {
+  ): Promise<UpdateResult> {
     return this.usersService.updateUser(id, updateUserDto);
   }
 }
