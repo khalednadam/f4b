a34e390c3417234a06ef25ea232428af0a1bf5f5
Khaled Nadam <khalednadam@gmail.com>
2024-08-22 20:56:32 +0300

    feat(Project): :sparkles: add project service
    added the services for the project to be used in the controller

--- src/projects/projects.service.ts
+++ src/projects/projects.service.ts
@@ -1,8 +1,14 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { Project } from './project.entity';
-import { Repository } from 'typeorm';
+import { DeleteResult, Repository, UpdateResult } from 'typeorm';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UpdateProjectDto } from './dto/update-project.dto';
+import {
+  IPaginationOptions,
+  paginate,
+  Pagination,
+} from 'nestjs-typeorm-paginate';
 
 @Injectable()
 export class ProjectsService {
@@ -11,9 +17,81 @@ export class ProjectsService {
     private readonly projectRepository: Repository<Project>,
   ) {}
 
-  async create(createProjectDto: CreateProjectDto): Promise<Project> {
+  /**
+   * Create a new project
+   */
+  async create(
+    createProjectDto: CreateProjectDto,
+    userId: number,
+  ): Promise<Project> {
     const project = new Project();
-    Object.assign(project, createProjectDto);
+    Object.assign(project, { ...createProjectDto, user: userId });
     return this.projectRepository.save(project);
+  }
+
+  /**
+   * Update a project by id
+   * @param projectId
+   * @param updateProjectDto the project body
+   */
+  async update(
+    projectId: number,
+    updateProjectDto: UpdateProjectDto,
+  ): Promise<UpdateResult> {
+    return this.projectRepository.update({ id: projectId }, updateProjectDto);
+  }
+
+  /**
+   * paginate the projects of a user using the user id
+   * @param options pagination options
+   * @param userId
+   */
+  async paginateByUser(
+    options: IPaginationOptions,
+    userId: number,
+  ): Promise<Pagination<Project>> {
+    return paginate<Project>(this.projectRepository, options, {
+      id: userId,
+      relations: {
+        user: true,
+      },
+    });
+  }
+
+  /**
+   * paginate all the projects
+   * @param options pagination options
+   */
+  async getAll(options: IPaginationOptions): Promise<Pagination<Project>> {
+    return paginate<Project>(this.projectRepository, options, {
+      relations: {
+        user: true,
+      },
+    });
+  }
+
+  /**
+   * delete a project by id
+   * @param id project id
+   */
+  async delete(id: number): Promise<DeleteResult> {
+    return this.projectRepository.delete({ id });
+  }
+
+  /**
+   * get a project by id
+   * @param id project id
+   */
+  async getOne(id: number): Promise<Project> {
+    const project = await this.projectRepository.findOne({
+      where: { id },
+      relations: {
+        user: true,
+      },
+    });
+    if (!project) {
+      throw new UnauthorizedException('Project is not found');
+    }
+    return project;
   }
 }
