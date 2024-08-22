74eb3fe7cef5bb96e42deceea40cd14ac9f6be14
Khaled Nadam <khalednadam@gmail.com>
2024-08-22 20:58:03 +0300

    feat(Project): :sparkles: add get controller
    added get controller for project which gets the project by id, more controllers to implement...

--- src/projects/projects.controller.ts
+++ src/projects/projects.controller.ts
@@ -1,4 +1,13 @@
-import { Body, Controller, Post, Request, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Param,
+  Post,
+  Request,
+  UnauthorizedException,
+  UseGuards,
+} from '@nestjs/common';
 import { CreateProjectDto } from './dto/create-project.dto';
 import { Project } from './project.entity';
 import { ProjectsService } from './projects.service';
@@ -8,13 +17,23 @@ import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
 export class ProjectsController {
   constructor(private readonly projectsService: ProjectsService) {}
 
-  /**
-   * Create a new project
-   */
   @UseGuards(JwtAuthGuard)
   @Post()
-  create(@Body() createProjectDto: CreateProjectDto, @Request() req): any {
-    console.log(req.user);
-    // return this.projectsService.create(createProjectDto);
+  async create(
+    @Body() createProjectDto: CreateProjectDto,
+    @Request() req,
+  ): Promise<any> {
+    return this.projectsService.create(createProjectDto, req.user.id);
   }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('project/:id')
+  async getOne(@Param('id') id: number): Promise<Project> {
+    return this.projectsService.getOne(id);
+  }
+
+  // @Get()
+  // async getAll() {
+  //   return await this.projectsService.getAll();
+  // }
 }
