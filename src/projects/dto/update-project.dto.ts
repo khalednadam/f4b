a1b9028b114849563c308e1bd090dfdc05c37ae3
Khaled Nadam <khalednadam@gmail.com>
2024-08-23 19:11:40 +0300

    fix(Project): :bug: make update projcet dto optional

--- src/projects/dto/update-project.dto.ts
+++ src/projects/dto/update-project.dto.ts
@@ -1,16 +1,20 @@
-import { IsEnum, IsString, IsUrl } from 'class-validator';
+import { IsEnum, IsOptional, IsString, IsUrl } from 'class-validator';
 import { PROJECT_TYPE } from 'src/constants/api.enums';
 
 export class UpdateProjectDto {
   @IsString()
+  @IsOptional()
   name: string;
 
   @IsString()
+  @IsOptional()
   description: string;
 
   @IsEnum(PROJECT_TYPE)
+  @IsOptional()
   type: string;
 
   @IsUrl()
+  @IsOptional()
   repositoryLink: string;
 }
